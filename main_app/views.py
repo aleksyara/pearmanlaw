85d6eecb5acf6d2f19de1e77701cdabcff21f6ea
aleksyara <aleksei.yarantsev@gmail.com>
2021-04-05 23:18:40 -0700

    complete mapping site. Add all routes

--- main_app/views.py
+++ main_app/views.py
@@ -12,7 +12,52 @@ def home(request):
 #   return HttpResponse('<h1>This is About Page</h1>') # simmiluar to res.send
 
 def resume_arbitrator(request):
-    return render(request, 'resume_arbitrator.html') #instead of HTTPResponse we can use a template
+    return render(request, 'resume_arbitrator.html') 
+
+def resume(request):
+    return render(request, 'resume.html') 
+
+def clients(request):
+    return render(request, 'clients.html') 
+
+def areasofpractice(request):
+    return render(request, 'areasofpractice.html') 
+
+def articles(request):
+    return render(request, 'articles.html') 
+
+def links(request):
+    return render(request, 'links.html')     
+
+def contactus(request):
+    return render(request, 'contactus.html')    
+
+def prac_business(request):
+    return render(request, 'prac_business.html') 
+
+def prac_publicworks(request):
+    return render(request, 'prac_publicworks.html')  
+
+def prac_transpo(request):
+    return render(request, 'prac_transpo.html')  
+
+def prac_realestate(request):
+    return render(request, 'prac_realestate.html')   
+
+def prac_finance(request):
+    return render(request, 'prac_finance.html')  
+
+def prac_construction(request):
+    return render(request, 'prac_construction.html')  
+
+def prac_publicagencies(request):
+    return render(request, 'prac_publicagencies.html')  
+
+def prac_utilities(request):
+    return render(request, 'prac_utilities.html')  
+
+def prac_litigation(request):
+    return render(request, 'prac_litigation.html')  
 
 def post(request):
     return render(request, 'post.html')
